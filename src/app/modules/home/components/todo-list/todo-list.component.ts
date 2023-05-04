f0214a99f1ea536ad69bd653a0dee55a6c7a1d5e
Bruno Tomaz <stomaz.bruno@gmail.com>
2023-05-03 22:15:25 -0300

    initial commit

---
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss'],
+})
+export class TodoListComponent implements OnInit {
+  public taskList: Array<TaskList> = [];
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public setEmitTaskList(event: string) {
+    this.taskList.push({ task: event, checked: false});
+  }
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm(
+      'Deseja realmente excluir todas as tarefas?'
+    );
+
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
+}
